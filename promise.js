738819664b74c36da2ebbfecb2a10d84bddd44cc
dimasrendyf <dimasrendy55@gmail.com>
2022-06-12 21:45:36 +0700

    first commit

--- promise.js
+++ promise.js
@@ -1,7 +1,35 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (result) => {
+  const resultIXX = await promiseTheaterIXX();
+  const resultVGC = await promiseTheaterVGC();
+  let marah = 0;
+  let tdkMarah = 0;
+
+  for (let i = 0; i < resultIXX.length; i++) {
+    if (resultIXX[i].hasil === 'marah') {
+      marah++;
+    } else {
+      tdkMarah++;
+    }
+  }
+
+  for (let i = 0; i < resultVGC.length; i++) {
+    if (resultVGC[i].hasil === 'marah') {
+        marah++;
+    } else {
+        tdkMarah++;
+    }
+}
+
+if (result === 'marah') {
+  return marah;
+} else {
+  return tdkMarah;
+}
+
+};
 
 module.exports = {
   promiseOutput,
